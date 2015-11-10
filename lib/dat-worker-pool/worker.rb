60b6c2df5a96204dc7743a23ff64816185a55985
Collin Redding <TempestTTU@gmail.com>
2015-11-10 12:39:17 -0600

    Add the rest of the `Worker` logic from `DefaultWorker`
    This adds the rest of the logic from `DefaultWorker` to the
    `Worker` mixin. This fills out the worker with all the logic
    needed to define custom workers.
    
    The `Worker` mixin now contains all the logic needed to define
    custom workers. It knows how to start, shutdown and process work
    items while running. Most of the logic was copied straight out of
    the default worker but a few things were changed:
    
    * The mixin now expects a `work!` method to be defined. This is
      tells it how to process a work item. It doesn't accept an on
      work proc.
    * Switch from a shutdown flag to a running flag. This is consistent
      with queues and makes the default state of the worker not running
      (shutdown).
    * This changes the `running?` method to be based on the running
      flag instead of if the thread is alive. This is for consistency
      with queues. This adds a `thread_alive?` to check if the thread
      is alive. This gives us the ability to ask if the worker has been
      shutdown even if its thread is still alive (meaning its in the
      process of shutting down).
    
    This also redoes the unit tests for the logic that was moved to the
    `Worker` mixin. They have been improved with more detailed and
    granular testing of the worker behavior. They also use our latest
    testing conventions.
    
    Finally, this removes most of the logic from `DefaultWorker` only
    leaving its `on_work` proc. It now uses the `work!` interface that
    the `Worker` mixin expects and calls its `on_work` proc.

--- lib/dat-worker-pool/worker.rb
+++ lib/dat-worker-pool/worker.rb
@@ -41,7 +41,93 @@ class DatWorkerPool
 
     module InstanceMethods
 
+      def initialize(runner, queue)
+        @runner  = runner
+        @queue   = queue
+        @running = false
+        @thread  = nil
+      end
+
+      def work(work_item)
+        run_callback('before_work', work_item)
+        work!(work_item)
+        run_callback('after_work', work_item)
+      end
+
+      def start
+        @running = true
+        @thread ||= Thread.new{ work_loop }
+      end
+
+      def shutdown
+        @running = false
+      end
+
+      def running?
+        !!@running
+      end
+
+      def shutdown?
+        !self.running?
+      end
+
+      # this is needed because even if the running flag has been set to false
+      # (meaning the worker has been shutdown) the thread may still be alive
+      # because its `work` is taking a long time or its still trying to shut
+      # down
+      def thread_alive?
+        !!(@thread && @thread.alive?)
+      end
+
+      def join(*args)
+        @thread.join(*args) if self.thread_alive?
+      end
+
+      def raise(*args)
+        @thread.raise(*args) if self.thread_alive?
+      end
+
       private
+
+      # overwrite this method to add custom work logic; this has to be
+      # overwritten or the workers will not know how to handle a work item
+      def work!(work_item)
+        raise NotImplementedError
+      end
+
+      # rescue `ShutdownError` and ignore it, its already been passed to the
+      # on-error callbacks in `fetch_and_work`; the shutdown error is just used
+      # to force the worker to exit its infinite loop if the worker pool is
+      # forcing itself to shutdown
+      def work_loop
+        run_callback 'on_start'
+        fetch_and_work while self.running?
+      rescue ShutdownError
+      ensure
+        run_callback 'on_shutdown'
+        @runner.despawn_worker(self)
+        @thread = nil
+      end
+
+      # rescue `ShutdownError` but re-raise it after calling the on-error
+      # callbacks, this ensures it causes the loop in `work_loop` to exit
+      def fetch_and_work
+        @runner.increment_worker_waiting
+        run_callback 'on_sleep'
+        work_item = @queue.pop
+        run_callback 'on_wakeup'
+        @runner.decrement_worker_waiting
+        work(work_item) if work_item
+      rescue ShutdownError => exception
+        handle_exception(exception, work_item)
+        Thread.current.raise exception
+      rescue StandardError => exception
+        handle_exception(exception, work_item)
+      end
+
+      def handle_exception(exception, work_item = nil)
+        run_callback('on_error', exception, work_item)
+      end
 
       def run_callback(callback, *args)
         (self.class.send("#{callback}_callbacks") || []).each do |callback|
--- test/unit/default_worker_tests.rb
+++ test/unit/default_worker_tests.rb
@@ -21,215 +21,32 @@ class DatWorkerPool::DefaultWorker
 
   end
 
-  class InitSetupTests < UnitTests
-    desc "when init"
+  class InitTests < UnitTests
     setup do
       @queue  = DatWorkerPool::DefaultQueue.new.tap(&:start)
       @runner = DatWorkerPool::Runner.new(:queue => @queue)
-    end
-    teardown do
-      @worker.shutdown if @worker
-      @queue.shutdown
-      @worker.join if @worker
-    end
-    subject{ @worker }
 
-  end
-
-  class InitTests < InitSetupTests
-    setup do
       @work_done = []
-      @worker = @worker_class.new(@runner, @queue).tap do |w|
-        w.on_work = proc{ |work| @work_done << work }
-      end
-    end
-
-    should have_accessors :on_work
-    should have_imeths :start, :shutdown, :join, :raise, :running?
-
-    should "start a thread with it's work loop using `start`" do
-      thread = nil
-      assert_nothing_raised{ thread = subject.start }
-      assert_instance_of Thread, thread
-      assert thread.alive?
-      assert subject.running?
-    end
-
-    should "call the block it's passed when it get's work from the queue" do
-      subject.start
-      @queue.push 'one'
-      subject.join 0.1 # trigger the worker's thread to run
-      @queue.push 'two'
-      subject.join 0.1 # trigger the worker's thread to run
-      assert_equal [ 'one', 'two' ], @work_done
-    end
-
-    should "increment and decrement its runners workers waiting count" do
-      increment_call_count = 0
-      Assert.stub(@runner, :increment_worker_waiting){ increment_call_count += 1 }
-      decrement_call_count = 0
-      Assert.stub(@runner, :decrement_worker_waiting){ decrement_call_count += 1 }
-
-      subject.start
-      @queue.push Factory.string
-
-      assert_equal 2, increment_call_count
-      assert_equal 1, decrement_call_count
-    end
-
-    should "flag itself for exiting it's work loop using `shutdown` and " \
-           "end it's thread once it's queue is shutdown" do
-      thread = subject.start
-      subject.join 0.1 # trigger the worker's thread to run, allow it to get into it's
-                       # work loop
-      assert_nothing_raised{ subject.shutdown }
-      @queue.shutdown
-
-      subject.join 0.1 # trigger the worker's thread to run, should exit
-      assert_not thread.alive?
-      assert_not subject.running?
-    end
-
-    should "despawn itself from its runner when shutdown" do
-      despawned_worker = nil
-      Assert.stub(@runner, :despawn_worker){ |worker| despawned_worker = worker }
-
-      subject.start
-      subject.join 0.1 # trigger the worker's thread to run
-      assert_nothing_raised{ subject.shutdown }
-      @queue.shutdown
-
-      assert_same subject, despawned_worker
-    end
-
-    should "raise an error on the thread using `raise`" do
-      subject.on_work = proc do |work|
-        begin
-          sleep 1
-        rescue RuntimeError => error
-          @work_done << error
-          raise error
-        end
-      end
-      subject.start
-      @queue.push 'a'
-      subject.join 0.1 # trigger the worker's thread to run
-
-      exception = RuntimeError.new
-      subject.raise exception
-      assert_equal [exception], @work_done
-    end
-
-  end
-
-  class CallbacksTests < InitSetupTests
-    setup do
-      @call_counter = 0
-
-      @on_start_called_with = nil
-      @on_start_called_at   = nil
-      @worker_class.on_start do |*args|
-        @on_start_called_with = args
-        @on_start_called_at   = (@call_counter += 1)
-      end
-
-      @on_shutdown_called_with = nil
-      @on_shutdown_called_at   = nil
-      @worker_class.on_shutdown do |*args|
-        @on_shutdown_called_with = args
-        @on_shutdown_called_at   = (@call_counter += 1)
-      end
-
-      @on_sleep_called_with = nil
-      @on_sleep_called_at   = nil
-      @worker_class.on_sleep do |*args|
-        @on_sleep_called_with = args
-        @on_sleep_called_at   = (@call_counter += 1)
-      end
-
-      @on_wakeup_called_with = nil
-      @on_wakeup_called_at   = nil
-      @worker_class.on_wakeup do |*args|
-        @on_wakeup_called_with = args
-        @on_wakeup_called_at   = (@call_counter += 1)
-      end
-
-      @on_error_called_with = nil
-      @worker_class.on_error{ |*args| @on_error_called_with = args }
-
-      @before_work_called_with = nil
-      @before_work_called_at   = nil
-      @worker_class.before_work do |*args|
-        @before_work_called_with = args
-        @before_work_called_at   = (@call_counter += 1)
-      end
-
-      @after_work_called_with  = nil
-      @after_work_called_at    = nil
-      @worker_class.after_work do |*args|
-        @after_work_called_with = args
-        @after_work_called_at   = (@call_counter += 1)
-      end
 
       @worker = @worker_class.new(@runner, @queue)
+      @worker.on_work = proc{ |work_item| @work_done << work_item }
+    end
+    teardown do
+      @worker.shutdown
+      @queue.shutdown
+      @worker.join
     end
     subject{ @worker }
 
-    should "pass its self to its start, shutdown, sleep and wakeup callbacks" do
+    should have_accessors :on_work
+
+    should "call the block it's passed when it gets work from the queue" do
       subject.start
-      @queue.push('work')
-      subject.shutdown
-      @queue.shutdown
+      work_items = Factory.integer(3).times.map{ Factory.string }
+      work_items.each{ |work_item| @queue.push(work_item) }
 
-      assert_equal [subject], @on_start_called_with
-      assert_equal [subject], @on_shutdown_called_with
-      assert_equal [subject], @on_sleep_called_with
-      assert_equal [subject], @on_wakeup_called_with
-    end
-
-    should "pass its self and work to its before and after work callbacks" do
-      subject.start
-      @queue.push('work')
-      subject.shutdown
-      @queue.shutdown
-
-      assert_equal [subject, 'work'], @before_work_called_with
-      assert_equal [subject, 'work'], @after_work_called_with
-    end
-
-    should "call its callbacks throughout its lifecycle" do
-      subject.start
-      assert_equal 1, @on_start_called_at
-      assert_equal 2, @on_sleep_called_at
-      @queue.push('work')
-      assert_equal 3, @on_wakeup_called_at
-      assert_equal 4, @before_work_called_at
-      assert_equal 5, @after_work_called_at
-      assert_equal 6, @on_sleep_called_at
-      subject.shutdown
-      @queue.shutdown
-      assert_equal 7, @on_wakeup_called_at
-      assert_equal 8, @on_shutdown_called_at
-    end
-
-    should "call its error callbacks when an exception occurs" do
-      exception = RuntimeError.new
-      subject.on_work = proc{ raise exception }
-      thread = subject.start
-      @queue.push('work')
-      assert_equal [subject, exception, 'work'], @on_error_called_with
-      assert_true thread.alive?
-    end
-
-    should "call its error callbacks when an shutdown error occurs and reraise" do
-      exception = DatWorkerPool::ShutdownError.new
-      subject.on_work = proc{ raise exception }
-      thread = subject.start
-      @queue.push('work')
-      assert_equal [subject, exception, 'work'], @on_error_called_with
-      assert_false thread.alive?
-      # ensure the shutdown error is handled and isn't thrown when we join
-      assert_nothing_raised{ thread.join }
+      subject.join 0.1 # trigger the workers thread to run
+      assert_equal work_items, @work_done
     end
 
   end
