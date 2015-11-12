5306144352299bcfd6c7d91f24c9de2243ad8d72
Collin Redding <TempestTTU@gmail.com>
2015-11-12 14:15:25 -0600

    Update `WorkerPoolSpy` and add `Worker::TestHelpers`
    This updates the worker pool spy with all the changes that were
    made to the worker pool. This also adds worker test helpers for
    help testing custom workers.
    
    The worker pool spy is a test helper that can be stubbed in as a
    replacement to a real worker pool. This updates it so it continues
    to provide the same interface as a real worker pool. This keeps it
    relevant and useful as a test helper.
    
    This also adds worker test helpers that help with building a worker
    and running its behavior. The test runner it builds can be used to
    run the workers callbacks and work method separately or all
    together. This should help with testing custom worker logic and
    making sure it works as expected.

--- lib/dat-worker-pool/worker_pool_spy.rb
+++ lib/dat-worker-pool/worker_pool_spy.rb
@@ -2,37 +2,33 @@ class DatWorkerPool
 
   class WorkerPoolSpy
 
-    attr_reader :num_workers, :debug
-    attr_reader :work_proc, :work_items
-    attr_reader :start_called
-    attr_reader :shutdown_called, :shutdown_timeout
-    attr_reader :on_queue_pop_callbacks, :on_queue_push_callbacks
-    attr_reader :on_worker_error_callbacks
-    attr_reader :on_worker_start_callbacks, :on_worker_shutdown_callbacks
-    attr_reader :on_worker_sleep_callbacks, :on_worker_wakeup_callbacks
-    attr_reader :before_work_callbacks, :after_work_callbacks
+    attr_reader :logger, :queue
+    attr_reader :options, :num_workers, :worker_class, :worker_params
+    attr_reader :work_items
+    attr_reader :start_called, :shutdown_called, :shutdown_timeout
     attr_accessor :worker_available
 
-    def initialize(num_workers = 1, debug = false, &block)
-      @num_workers = num_workers
-      @debug       = debug
-      @work_proc   = block
+    def initialize(worker_class, options = nil)
+      @worker_class = worker_class
+      if !@worker_class.kind_of?(Class) || !@worker_class.include?(DatWorkerPool::Worker)
+        raise ArgumentError, "worker class must include `#{DatWorkerPool::Worker}`"
+      end
+
+      @options      = options
+      @num_workers  = @options[:num_workers].to_i
+      if @num_workers && @num_workers < MIN_WORKERS
+        raise ArgumentError, "number of workers must be at least #{MIN_WORKERS}"
+      end
+
+      @logger        = @options[:logger]
+      @queue         = @options[:queue]
+      @worker_params = @options[:worker_params]
 
       @worker_available = false
-      @work_items = []
-      @start_called = false
+      @work_items       = []
+      @start_called     = false
       @shutdown_called  = false
       @shutdown_timeout = nil
-
-      @on_queue_pop_callbacks       = []
-      @on_queue_push_callbacks      = []
-      @on_worker_error_callbacks    = []
-      @on_worker_start_callbacks    = []
-      @on_worker_shutdown_callbacks = []
-      @on_worker_sleep_callbacks    = []
-      @on_worker_wakeup_callbacks   = []
-      @before_work_callbacks        = []
-      @after_work_callbacks         = []
     end
 
     def start
@@ -40,39 +36,22 @@ class DatWorkerPool
     end
 
     def shutdown(timeout = nil)
-      @shutdown_called = true
+      @shutdown_called  = true
       @shutdown_timeout = timeout
     end
 
-    def add_work(work)
-      return unless work
-      @work_items << work
-      @on_queue_push_callbacks.each(&:call)
+    def add_work(work_item)
+      return if work_item.nil?
+      @work_items << work_item
     end
 
-    def pop_work
-      work = @work_items.shift
-      @on_queue_pop_callbacks.each(&:call)
-      work
-    end
-
-    def queue_empty?
-      @work_items.empty?
+    def waiting
+      @num_workers
     end
 
     def worker_available?
-      @worker_available
+      !!@worker_available
     end
-
-    def on_queue_pop(&block);       @on_queue_pop_callbacks       << block; end
-    def on_queue_push(&block);      @on_queue_push_callbacks      << block; end
-    def on_worker_error(&block);    @on_worker_error_callbacks    << block; end
-    def on_worker_start(&block);    @on_worker_start_callbacks    << block; end
-    def on_worker_shutdown(&block); @on_worker_shutdown_callbacks << block; end
-    def on_worker_sleep(&block);    @on_worker_sleep_callbacks    << block; end
-    def on_worker_wakeup(&block);   @on_worker_wakeup_callbacks   << block; end
-    def before_work(&block);        @before_work_callbacks        << block; end
-    def after_work(&block);         @after_work_callbacks         << block; end
 
   end
 
